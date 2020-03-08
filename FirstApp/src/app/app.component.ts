98fadf984af0c1917a12328aa8b9732b809362c4
Mardoek Thienpondt <Mardoek.Thienpondt.y9250@student.hogent.be>
2020-03-08 03:26:37 +0100

    2de commit 8 feb 3:26

--- FirstApp/src/app/app.component.ts
+++ FirstApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstApp';
+
+  randomNumber: number = Math.floor(Math.random() * 100) + 1;
+  teller: number = 0;
+  
+
+  gok(getal) {
+    if(this.teller < 10){
+    this.teller++;
+    window.alert(this.teller);
+    
+    if(getal > this.randomNumber){window.alert('Lager')};
+    if(getal < this.randomNumber){window.alert('Hoger')};
+    if(getal == this.randomNumber){window.alert('Ge zit er bal op!')};
+  }
+  else(window.alert('you lost'));
+  }
 }
+
+

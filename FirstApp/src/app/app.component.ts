d03a3557ad09e12bc6033d096eb44f2b20c4c253
Mardoek Thienpondt <Mardoek.Thienpondt.y9250@student.hogent.be>
2020-03-16 04:57:07 +0100

    kleine aanpassingen omstreeks de pogingen 16 maart 4:57

--- FirstApp/src/app/app.component.ts
+++ FirstApp/src/app/app.component.ts
@@ -19,11 +19,14 @@ export class AppComponent {
   gok(getal) {
     if(this.teller < 10){
     this.teller++;
-    window.alert(this.teller);
+    
     
     if(getal > this.randomNumber){window.alert('Lager')};
     if(getal < this.randomNumber){window.alert('Hoger')};
-    if(getal == this.randomNumber){window.alert('Ge zit er bal op!')};
+    if(getal == this.randomNumber){
+      window.alert('Ge zit er bal op!\nOpnieuw Spelen?')
+      window.location.reload();
+    };
   }
   else{
     window.alert('you lost, starting over...');

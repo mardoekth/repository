f50b8744b455a71e5c6ec5c56a790462a304c765
mardoekth <61159479+mardoekth@users.noreply.github.com>
2020-11-02 18:04:04 +0100

    manual commit
    tooltip module, input module

---
+++ FirstApp/material.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+@NgModule({
+        imports:[MatButtonModule, MatTooltipModule, ],
+        exports:[MatButtonModule, MatTooltipModule, ]
+    })
+export class MaterialModule {}
